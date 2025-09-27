79cefa1348902a1551777912664dd6415d9a83f0
franklin <njorogefranklin2@gmail.com>
2025-09-27 15:00:37 +0300

    Describe your changes

--- script.js/config.js
+++ script.js/config.js
@@ -1,2 +1,6 @@
-const API_BASE_URL = 'https://test2-production-41f3.up.railway.app';
+// config.js
+const API_BASE_URL = window.location.hostname.includes('localhost')
+  ? 'http://localhost:3000'
+  : 'https://test2-production-41f3.up.railway.app';
+
 const FRONTEND_URL = 'https://testfront2.onrender.com'; // Your primary frontend URL
