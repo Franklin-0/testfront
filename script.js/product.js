58871fec685c4f66503b609e4b2450f907651424
franklin <njorogefranklin2@gmail.com>
2025-09-28 10:09:15 +0300

    Describe your changes

--- script.js/product.js
+++ script.js/product.js
@@ -320,7 +320,7 @@ async function handleBuyNow(product) {
   }
 }
 
-function setupRelatedProductSlideshow() {
+export function setupRelatedProductSlideshow() {
   const container = document.querySelector('.related-slideshow-container');
   if (!container) return;
 
