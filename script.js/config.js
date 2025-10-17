04beb29cc8ff106fa8cb4b4d5eaf2aab601fc2bd
franklin <njorogefranklin2@gmail.com>
2025-10-17 16:03:36 +0300

    some changes

--- script.js/config.js
+++ script.js/config.js
@@ -5,12 +5,12 @@ const isLocalhost = window.location.hostname.includes("localhost");
 
 // Backend API base
 const API_BASE_URL = isLocalhost
-  ? "http://localhost:3000" // match the port where your backend runs locally
+  ? "http://localhost:1000" // your backend port
   : "https://test2-production-41f3.up.railway.app";
 
 // Frontend base (mostly useful for OAuth / CORS redirects)
 const FRONTEND_URL = isLocalhost
-  ? "http://localhost:3000" // your dev frontend
+  ? "http://localhost:5501" // your frontend port
   : "https://testfront2.onrender.com";
 
 export { API_BASE_URL, FRONTEND_URL };
